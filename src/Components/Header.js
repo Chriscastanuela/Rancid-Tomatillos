9285e03caef2b4cb92a366b78c7df5346ceb1167
Chriscastanuela <chriscastanuela@ymail.com>
2020-10-06 16:27:48 -0600

    Add grid to Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 
 export class Header extends Component {
+    state = {
+        username: "",
+        profilePicture: ""
+    }
+    
     render() {
         return (
-            <h1 className="Header">Rancid Tomatillos</h1>
+            <section style={sectionStyle}>
+                <h1 className="Header" style={headerStyle}>Rancid Tomatillos</h1>
+                <a className="Login">Login</a>
+            </section>
         )
     }
+}
+
+const sectionStyle = {
+    display: 'grid',
+    gridTemplateColumns: '3fr 1fr 1fr 1fr 1fr 1fr'
+}
+
+const headerStyle = {
+    gridColumn: '1'
 }
 
 export default Header
