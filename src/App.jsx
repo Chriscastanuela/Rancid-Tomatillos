35b07f6b2aaf3076889a1be210f3da2be84ca3e2
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-10-08 15:44:21 -0600

    Add a Main section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './css/App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Main from './Components/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
