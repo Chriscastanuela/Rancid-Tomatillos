3022139f9a47095c6ceb90ab819403cbcfe925d5
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-10-08 15:36:30 -0600

    Style Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <section>
-            <h1 style={sectionStyle}>Rancid</h1>
+        <section style={sectionStyle}>
+            <h1 style={headerStyle}>Rancid</h1>
         </section>
     )
 }
 
 const sectionStyle = {
     display: 'grid',
-    gridTemplateColumns: '1fr 1fr',
+    gridTemplateColumns: '1fr 1fr 1fr',
     borderStyle: 'solid',
     borderLeft: 'none',
     borderRight: 'none',
     borderTop: 'none',
     borderWidth: '1px',
     backgroundColor: '#fa5252'
+}
+
+const headerStyle = {
+    fontFamily: 'Kaushan Script, cursive',
+    fontSize: '30px',
+    gridColumn: '2',
+    alignSelf: 'center',
 }
