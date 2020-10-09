ea29630e6acf8c5b40b141b232fe4608140a17cb
Chriscastanuela <chriscastanuela@ymail.com>
2020-10-09 17:21:32 -0600

    Add list props

--- src/Components/ListSection.jsx
+++ src/Components/ListSection.jsx
@@ -14,62 +14,51 @@ export class ListSection extends Component {
       }
     
     componentDidMount() {
-    Promise.all([
-        fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-        .then(response => response.json())
-        .then(response => this.setState({movieData: response}))
-        .catch(err => alert('Data failed to load. Try again later', err))
-    ])
-    .then(promiseDotAllIndex => {
-        console.log(this.state.movieData);
-        this.getAList((
-        this.state.movieData.movies.sort((a,b) => {
-            return b.release_date - a.release_date;
+        Promise.all([
+            fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+            .then(response => response.json())
+            .then(response => this.setState({movieData: response}))
+            .catch(err => alert('Data failed to load. Try again later', err))
+        ])
+        .then(promiseDotAllIndex => {
+            console.log(this.state.movieData);
+            this.getAList((
+            this.state.movieData.movies.sort((a,b) => {
+                return b.release_date - a.release_date;
+            })
+            ), this.recentMovies);
+            this.getAList((
+            this.state.movieData.movies.sort((a,b) => {
+                return a.average_rating - b.average_rating;
+            })
+            ), this.lowestRated);
+            this.getAList((
+            this.state.movieData.movies.sort((a,b) => {
+                return b.average_rating - a.average_rating;
+            })
+            ), this.highestRated);
+            //
         })
-        ), this.recentMovies);
-        this.getAList((
-        this.state.movieData.movies.sort((a,b) => {
-            return a.average_rating - b.average_rating;
-        })
-        ), this.lowestRated);
-        this.getAList((
-        this.state.movieData.movies.sort((a,b) => {
-            return b.average_rating - a.average_rating;
-        })
-        ), this.highestRated);
-    })
     }
     
     getAList(theList, theKey) {
-    console.log('I exist, am I before or after?')
-    let theTen = [];
-    theList.forEach(index => {
-        if (theTen.length < 10) {
-        theTen.push(index);
-        }
-    })
-    this.setState({theKey: theTen})
-    console.log(this.state.theKey, 'asdfsafdfasfdafa')
+        console.log('I exist, am I before or after?')
+        let theTen = [];
+        theList.forEach(index => {
+            if (theTen.length < 10) {
+            theTen.push(index);
+            }
+        })
+        this.setState({theKey: theTen})
+        console.log(this.state.theKey, 'asdfsafdfasfdafa')
     }
     
     render() {
         return (
             <section style={listSectionStyle}>
-                <List />
-                <List />
-                <List />
-                {/* <div style={firstDivStyle}>
-                    <div className='TheList' style={listStyle} id='firstList'>
-                    </div>
-                </div>
-                <div>
-                    <div className='TheList' style={listStyle} id='secondList'>
-                    </div>
-                </div>
-                <div>
-                    <div className='TheList' style={listStyle} id='thirdList'>
-                    </div>
-                </div> */}
+                <List list={this.state.recentMovies}/>
+                <List list={this.state.lowestRated}/>
+                <List list={this.state.highestRated}/>
             </section>
         )
     }
