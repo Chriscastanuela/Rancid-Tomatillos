425c844e60b568ab2195f8f4b0dd7065769babe6
Chriscastanuela <chriscastanuela@ymail.com>
2020-10-09 16:35:29 -0600

    Add list style

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -4,17 +4,49 @@ export class Lists extends Component {
     render() {
         return (
             <section style={listSectionStyle}>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div style={firstDivStyle}>
+                    <div style={listStyle}>
+                        <ul>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div style={listStyle}>
+                        <ul>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div style={listStyle}>
+                        <ul>
+                        </ul>
+                    </div>
+                </div>
             </section>
         )
     }
 }
 
+let firstDivStyle = {
+    paddingTop: '15px'
+}
+
 let listSectionStyle = {
     display: 'grid',
-    gridTemplateRows: '1fr 1fr 1fr'
+    gridTemplateRows: '1fr 1fr 1fr',
+    justifyContent: 'center',
+    alignContent: 'center',
+}
+
+let listStyle = {
+    margin: '15px',
+    width: '300px',
+    height: '300px',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    borderRadius: '25%',
+    backgroundColor: '#f3eed9',
+    boxShadow: '5px 10px 30px #888888'
 }
 
 export default Lists
