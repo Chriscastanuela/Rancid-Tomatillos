e8e0544873a97d2f1da6b14ce5e1e8474b08b84d
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-10-12 16:23:00 -0600

    change grid to bootstrap settings and set div widths

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,17 +4,19 @@ import Movies from './Movies.jsx'
 
 export default function Main() {
   return (
-    <section style={mainSectionStyle}>
+    <section>
       <ListSection />
       <Movies />
     </section>
   )
 }
 
-let mainSectionStyle = {
-  display: 'grid',
-  gridTemplateRows: '1fr 1fr'
-}
+
+
+// let mainSectionStyle = {
+//   display: 'grid',
+//   gridTemplateRows: '1fr 1fr'
+// }
 
 // getRecentMovies() {
   //   console.log('I exist, am I before or after?')
