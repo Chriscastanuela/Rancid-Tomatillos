573c4a19a5fc0f5c813a699b85175b6189f5c30d
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-10-20 13:55:23 -0600

    Add Login test

---
+++ src/Components/Login.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './Login';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Login', () => {
+   
+    it('Should work', () => {
+      expect(true).toBeTruthy()
+    })
+})
