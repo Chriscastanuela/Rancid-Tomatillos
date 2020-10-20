e16753c157d71b5d5022db922b44bb77255a4039
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-10-20 13:59:28 -0600

    Add movie card test

---
+++ src/Components/MovieCard.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MovieCard from './MovieCard';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('MovieCard', () => {
+   
+    it('Should work', () => {
+      expect(true).toBeTruthy()
+    })
+})
