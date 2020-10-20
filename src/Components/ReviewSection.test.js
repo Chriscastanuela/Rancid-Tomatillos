56340992a6676ae656b5a897c4a84dd45ccfb8fe
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-10-20 14:07:08 -0600

    Add ReviewSection test

---
+++ src/Components/ReviewSection.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReviewSection from './ReviewSection';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('ReviewSection', () => {
+   
+    it('Should work', () => {
+      expect(true).toBeTruthy()
+    })
+})
