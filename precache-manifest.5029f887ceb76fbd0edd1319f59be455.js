0ea83e60c790f0920a0230ccde55352e7585beaa
Chriscastanuela <chriscastanuela@ymail.com>
2020-10-21 06:27:29 -0600

    Updates

---
+++ precache-manifest.5029f887ceb76fbd0edd1319f59be455.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f474a4d433ccc7fdecd6618ed3dd14f5",
+    "url": "/http//Chriscastanuela.github.io/Rancid-Tomatillos/index.html"
+  },
+  {
+    "revision": "fa02e8c60fd4907492dd",
+    "url": "/http//Chriscastanuela.github.io/Rancid-Tomatillos/static/css/main.02151bf1.chunk.css"
+  },
+  {
+    "revision": "07a45cf31b3f7b47ccfa",
+    "url": "/http//Chriscastanuela.github.io/Rancid-Tomatillos/static/js/2.faec2a1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/http//Chriscastanuela.github.io/Rancid-Tomatillos/static/js/2.faec2a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa02e8c60fd4907492dd",
+    "url": "/http//Chriscastanuela.github.io/Rancid-Tomatillos/static/js/main.cb84dc91.chunk.js"
+  },
+  {
+    "revision": "d6eec32473f7270ffd41",
+    "url": "/http//Chriscastanuela.github.io/Rancid-Tomatillos/static/js/runtime-main.3540658d.js"
+  },
+  {
+    "revision": "da31ec9b688221bbf8d6a36e75454d9d",
+    "url": "/http//Chriscastanuela.github.io/Rancid-Tomatillos/static/media/user.da31ec9b.png"
+  }
+]);
