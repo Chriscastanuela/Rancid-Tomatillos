96e7b02060b82bea3dde651702c7d65b2b79b56d
Chriscastanuela <chriscastanuela@ymail.com>
2020-10-21 06:43:30 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/http//chriscastanuela.github.io/rancid-tomatillos/precache-manifest.bb798ed1748c69a01d903dc8fd45ff1a.js"
+  "./precache-manifest.05b93b2f34c334a9c2fefc8c84b26063.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/http//chriscastanuela.github.io/rancid-tomatillos/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
